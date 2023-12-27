f7eab149a8c27348f62fba74b37d5e5a5649d198
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2023-12-27 19:16:47 +0000

    Question 2

--- src/main/kotlin/FundamentalsQuestions.kt
+++ src/main/kotlin/FundamentalsQuestions.kt
@@ -4,8 +4,25 @@ fun main() {
     val eveningNotification = 135
     printNotificationSummary(morningNotification)
     printNotificationSummary(eveningNotification)
+
+    // Q2
+    val child = 5
+    val adult = 28
+    val senior = 87
+    val isMonday = true
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
 }
 
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    return when(age) {
+        in 0..12 -> 15
+        in 13..60 -> if (isMonday) 25 else 30
+        in 61..100 -> 20
+        else -> -1
+    }
+}
 
 fun printNotificationSummary(numberOfMessages: Int) {
     if (numberOfMessages < 100) {
