d3e964479c0381431013726e4506bf9128b9ab1a
mateuszs6 <124052017+mateuszs6@users.noreply.github.com>
2023-12-27 19:22:35 +0000

    Question 4

--- src/main/kotlin/FundamentalsQuestions.kt
+++ src/main/kotlin/FundamentalsQuestions.kt
@@ -13,6 +13,40 @@ fun main() {
     println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
     println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
     println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+
+    // Q3
+    printFinalTemperature(27.0, "Celsius", "Fahrenheit") { 9.0 / 5.0 * it + 32 }
+    printFinalTemperature(350.0, "Kelvin", "Celsius") { it - 273.15 }
+    printFinalTemperature(10.0, "Fahrenheit", "Kelvin") { 5.0 / 9.0 * (it - 32) + 273.15 }
+
+    // Q4
+    val brunoSong = Song("Venom", "Eminem", 2018, 600_000_000)
+    brunoSong.printDescription()
+    println(brunoSong.isPopular)
+}
+
+class Song(
+    val title: String,
+    val artist: String,
+    val yearPublished: Int,
+    val playCount: Int
+) {
+    val isPopular: Boolean
+        get() = playCount >= 1000
+
+    fun printDescription() {
+        println("$title, performed by $artist, was released in $yearPublished.")
+    }
+}
+
+fun printFinalTemperature(
+    initialMeasurement: Double,
+    initialUnit: String,
+    finalUnit: String,
+    conversionFormula: (Double) -> Double
+) {
+    val finalMeasurement = String.format("%.2f", conversionFormula(initialMeasurement)) // two decimal places
+    println("$initialMeasurement degrees $initialUnit is $finalMeasurement degrees $finalUnit.")
 }
 
 fun ticketPrice(age: Int, isMonday: Boolean): Int {
